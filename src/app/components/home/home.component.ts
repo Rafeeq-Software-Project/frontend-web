01e6afa378af0378ec476562f937bbccaae9a19b
mohamed-ibrahem123 <mohamedm123t@gmail.com>
2026-03-05 07:57:37 +0200

    edit home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit, OnDestroy } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
 import { CommonModule } from '@angular/common';
+import { ThemeService } from '../../services/theme.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -10,8 +11,35 @@ import { CommonModule } from '@angular/common';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
   isMenuOpen = false;
+  isScrolled = false;
+  isDarkMode = false;
+  private themeSub!: Subscription;
+
+  constructor(private themeService: ThemeService) { }
+
+  ngOnInit(): void {
+    this.themeSub = this.themeService.isDarkMode$.subscribe(dark => {
+      this.isDarkMode = dark;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.themeSub?.unsubscribe();
+  }
+
+  toggleTheme(): void {
+    this.themeService.toggleTheme();
+  }
+
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    this.isScrolled = window.scrollY > 20;
+    if (this.isScrolled && this.isMenuOpen) {
+      this.isMenuOpen = false;
+    }
+  }
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;

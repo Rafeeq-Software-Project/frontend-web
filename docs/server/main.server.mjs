365158da8c572aabd5d0edabbd751ca75fcf3072
mohamed-ibrahem123 <mohamedm123t@gmail.com>
2026-03-05 08:49:03 +0200

    update link

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-Y3O7NXIE.mjs";import"./chunk-RWZIXBKS.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-ZBUTPGSG.mjs";import"./chunk-RWZIXBKS.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};

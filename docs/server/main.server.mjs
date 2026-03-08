748e853bb4aa835c3caeeda7d8616c8053cd93c9
mohamed-ibrahem123 <mohamedm123t@gmail.com>
2026-03-08 10:25:45 +0200

    update link

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-ZBUTPGSG.mjs";import"./chunk-RWZIXBKS.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-BUVZ7QTQ.mjs";import"./chunk-RWZIXBKS.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
